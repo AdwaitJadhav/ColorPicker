fe04a0a4db6e821161f45ed9d5ee5c531dc9073e
AdwaitJadhav <103038732+AdwaitJadhav@users.noreply.github.com>
2022-09-26 18:30:47 +0530

    Update random.js

--- random.js
+++ random.js
@@ -1,3 +1,5 @@
+
+
 function rand(){
     let disp=document.getElementById("display");
     let rand1=Math.floor((Math.random()*255));
@@ -11,3 +13,67 @@ function rand(){
     let hid=document.getElementById("int");
     hid.style.display="none";
 }
+
+function red(){
+    let disp=document.getElementById("display");
+    let rand2=Math.floor((Math.random()*255));
+    let rand3=Math.floor((Math.random()*255));
+    let rand4=1;
+    let rand1=255;
+    let color="rgba("+rand1+","+rand2+","+rand3+","+rand4+")";
+    disp.style.backgroundColor=color;
+    let val=document.getElementById("value");
+    val.innerHTML=color;
+    let hid=document.getElementById("int");
+    hid.style.display="none";
+}
+
+function blue(){
+    let disp=document.getElementById("display");
+    let rand1=Math.floor((Math.random()*255));
+    let rand2=Math.floor((Math.random()*255));
+    let rand4=1;
+    let rand3=255;
+    let color="rgba("+rand1+","+rand2+","+rand3+","+rand4+")";
+    disp.style.backgroundColor=color;
+    let val=document.getElementById("value");
+    val.innerHTML=color;
+    let hid=document.getElementById("int");
+    hid.style.display="none";
+}
+
+function green(){
+    let disp=document.getElementById("display");
+    let rand1=Math.floor((Math.random()*255));
+    let rand3=Math.floor((Math.random()*255));
+    let rand4=1;
+    let rand2=255;
+    let color="rgba("+rand1+","+rand2+","+rand3+","+rand4+")";
+    disp.style.backgroundColor=color;
+    let val=document.getElementById("value");
+    val.innerHTML=color;
+    let hid=document.getElementById("int");
+    hid.style.display="none";
+}
+
+/*$(document).ready(function(){
+    $("#range").mousedown(function(){
+        $("#ranop").slideToggle("fast");
+    });
+});
+*/
+function slidedown(){
+    let par=document.getElementById("range");
+    let li=document.getElementById("ranop");
+    if(li.style.display=="none"){
+        li.style.transitionProperty="display";
+        li.style.transitionDelay="2s";
+        li.style.display="block";
+    }
+    else{
+        li.style.transitionProperty="display";
+        li.style.transitionDelay="2s";
+        li.style.display="none";
+    }
+}
+
